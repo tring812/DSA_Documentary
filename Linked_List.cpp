c0ca450eb398eaed4759fada56d22f49e997b217
DuckTri <huutri812007@gmail.com>
2026-01-29 14:50:08 +0700

    Steps to create a linked list

---
+++ Linked_List.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node{
+  int value;
+  node *next;
+};
+
+struct List{
+  node *head, *tail;
+};
+
+void init(List &l){
+  l.head = nullptr, l.tail = nullptr;
+}
+
+void add_tail(List &l, int x){
+  node *p = get_node(x);
+}
+
+node *get_node(int x){
+  node *p = new node;
+  p->next = nullptr;
+  p->value = x;
+  return p;
+}
+
+int main(){
+  //create linked list
+  List l;
+  init(l);
+  int n; cin >> n;
+  for(int i = 0;i < n;++i){
+    int x; cin >> x;
+    add_tail(l, x);
+  }
+  return 0;
+}
